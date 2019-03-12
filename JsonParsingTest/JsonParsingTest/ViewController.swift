595ff1cc861cdcbe9273fba82d3e34295f9414c2
BaeJiHo <35207193+BaeJiho@users.noreply.github.com>
2019-03-12 14:27:18 +0900

    Add files via upload

---
+++ JsonParsingTest/JsonParsingTest/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  JsonParsingTest
+//
+//  Created by 배지호 on 12/03/2019.
+//  Copyright © 2019 baejiho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    var person: [String: Any] = [:]
+    var nameArr: [String] = []
+    var ageArr: [String] = []
+    var employedArr: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //번들에 있는 파일 경로를 가져오기
+        let path = Bundle.main.path(forResource: "person", ofType: "json")
+        //경로를 가지고 json파일을 데이터화 하기
+        guard let data = try! String(contentsOfFile: path!).data(using: .utf8) else {return}
+        //JSONSerializtion으로 파싱하기
+        let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+        person = json
+        guard let parsingData = person["person"] as? [[String: Any]] else {return}
+        for i in parsingData {
+            print(i["age"] as! String)
+        }
+//        if let d = person["person"] as? [[String: Any]] {
+//            for i in d {
+//                nameArr.append(i["name"] as! String)
+//            }
+//        }
+        print(nameArr)
+        
+    }
+}
+
